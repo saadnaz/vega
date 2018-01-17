0ac6fb6ee379c8221273e72935e0f4014ececbb4
saadnaz <ouldzein.saadna@gmail.com>
2018-01-17 11:29:04 +0100

    setting entity framework

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using vega.Persistence;
 
 namespace Vega
 {
@@ -28,6 +30,7 @@ namespace Vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer("..."));
             // Add framework services.
             services.AddMvc();
         }
